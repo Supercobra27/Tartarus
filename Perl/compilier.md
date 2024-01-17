e598aaa0bf3830b572c9aab33d396e1a96ab3122
Supercobra27 <silverbergryan@gmail.com>
2024-01-17 12:21:33 -0500

    perl

---
+++ Perl/compilier.md
@@ -0,0 +1,14 @@
+# Arguments for Compilier
+-d - Runs in debugger
+-IDirectory - Include directory
+-T - enable tainting checks
+-t - enables tainting warnings
+-U - allows unsafe ops
+-w - enables useful warnings
+-W - enables all warnings
+-X - disables all warnings
+-e program - runs perl script
+file - runs perl from a file
+
+# Run in Terminal
+perl (file path)
---
+++ Perl/helloWorld.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+#run scripts in terminal with perl [file path]
+
+print "Hello, World!\n";
+
+$foo = 10;
+
+print "value of a is $a\n";
+print 'value of a is $a\n';
+
+$var = <<"EOF";
+This is an example of multiline text that we can use with the << operator
+it is very nice
+EOF
+
+print "\"$var\"\n";
+
+$var = <<"EOF";
+Datatypes have 3 starters,
+scalars are $
+arrays are @
+hashes are %
+EOF
+
+print "$var\n";
+print "\aalert\n";
+
+print "now time for arrays\n";
+
+@ages = (24,30,40);
+
+print "$ages[0]\n";
+print "$ages[1]\n";
+print "@ages\n";
+
+print "hashes\n"
+
+%test = ('me',23,'you',42,'him',30);
+
+$data{'me'} = 44;
+
+print "%test\n";
+
+# stopped at arrays
+#https://www.tutorialspoint.com/perl/perl_arrays.htm
